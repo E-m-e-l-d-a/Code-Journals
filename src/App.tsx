5822a4adaa4b79c8c9a4620bd82a4b6aa7c97e1a
Emelda <marycynthiaugwu14@gmail.com>
2025-07-18 21:10:27 +0100

    Created a react+vite app, installed tailwind and reactrouter as dependencies

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className=" min-h-screen flex-center bg-[#34699A]">
+        <h1 className="text-4xl font-bold text-[#053158]">Hello people</h1>  
+      </div>
+    </>
+  )
+}
+
+export default App;
